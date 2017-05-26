14a504f23bd865c9d8d1bb977404b51c07f5412b
Anastasiia Bakuta <ae@anahoret.com>
2017-05-26 10:22:38 +0300

    Updated styles

--- components/app.ios.js
+++ components/app.ios.js
@@ -1,8 +1,8 @@
-import React, { Component }           from 'react'
-import { Text, TextInput, View }      from 'react-native';
-import PropTypes                      from 'prop-types'
-import { bindActionCreators }         from 'redux'
-import { connect }                    from 'react-redux'
+import React, { Component } from 'react'
+import { Text, TextInput, View, StyleSheet } from 'react-native'
+import PropTypes from 'prop-types'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 import { fetchCollection, addRecord } from 'react-luggage-redux'
 
 const mapStateToProps = state => ({
@@ -13,6 +13,35 @@ const mapDispatchToProps = dispatch => bindActionCreators({
   fetchCollection,
   addRecord
 }, dispatch)
+
+const baseFontSize = 16
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: 'rgba(245,243,243,1)',
+    height: null,
+    flex: 1,
+    flexDirection: 'column',
+    marginTop: 20,
+    padding: 20,
+    width: null
+  },
+  input: {
+    alignSelf: 'stretch',
+    backgroundColor: '#ffffff',
+    borderColor: 'rgba(0,0,0,0.3)',
+    borderWidth: 1,
+    height: 40,
+    marginBottom: 20,
+    paddingLeft: 10,
+    paddingRight: 10
+  },
+  item: {
+    color: '#000000',
+    fontSize: baseFontSize,
+    marginBottom: 5
+  }
+})
 
 export class App extends Component {
   static propTypes = {
@@ -38,16 +67,16 @@ export class App extends Component {
 
   render() {
     const { todos } = this.props
-    console.log(this.props)
-
     return (
-      <View>
-        <TextInput placeholder='New todo'
-                   ref={i => { this.todoInput = i }}
+      <View style={styles.body}>
+        <TextInput style={styles.input}
+                   placeholder='New todo'
                    onChangeText={(text) => this.setState({text})}
                    value={this.state.text}
                    onSubmitEditing={this.addTodo.bind(this)} />
-        { todos.map((todo, i) => <View key={i}><Text>{todo.text}</Text></View>) }
+        { todos.map((todo, i) => (
+          <View key={i}><Text style={styles.item}>{todo.text}</Text></View>
+        )) }
       </View>
     )
   }
