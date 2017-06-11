c4573a73a895fd2955022b75c3d532d5140ef56e
Anastasiia Bakuta <ae@anadeainc.com>
2017-06-11 23:22:44 +0300

    Refactoring

---
+++ components/index.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { luggageMiddleware } from 'react-luggage-redux'
+import DropboxAutent from 'dropbox-autent'
+import reducer from '../reducers/index'
+import createSessionManager from '../lib/createSessionManager'
+import TodoList from './TodoList'
+
+const API_KEY = 'tqx0ze13xl6vawf'
+
+class App extends Component {
+  static propTypes = {
+    token: PropTypes.string.isRequired
+  }
+
+  constructor(props) {
+    super(props)
+    const { token } = this.props
+    const SessionManager = createSessionManager(token)
+    this.store = createStore(
+      reducer,
+      applyMiddleware(luggageMiddleware({
+        apiKey: API_KEY,
+        SessionManager
+      }))
+    )
+  }
+
+  render() {
+    return (
+      <Provider store={this.store}>
+        <TodoList />
+      </Provider>
+    )
+  }
+}
+
+const WithDropboxAutent = DropboxAutent({apiKey: API_KEY, redirectUrl: 'oauth2todo://foo'})(App)
+export default WithDropboxAutent
